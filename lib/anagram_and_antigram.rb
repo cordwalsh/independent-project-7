ad0c2136776daab52dac7027cea811da0e080267
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-07-19 09:11:10 -0700

    set up class, setting up logic

---
+++ lib/anagram_and_antigram.rb
@@ -0,0 +1,15 @@
+class Anagram_and_Antigram
+  def initialize(word)
+    @word = word
+    @vowels = vowels
+    vowels = [a,e,i,o,u]
+    @newaray = []
+  end
+  def Anagram
+    @new_word = @word.split("")
+    
+  end
+
+
+
+end
