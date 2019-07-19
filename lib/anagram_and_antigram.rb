6cc29c131f8a21ca99abda47ba22eefa3861b776
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-07-19 14:26:23 -0700

    setting up third test

--- lib/anagram_and_antigram.rb
+++ lib/anagram_and_antigram.rb
@@ -3,33 +3,50 @@ class Anagram_and_Antigram
     @input = input
     @newaray = []
   end
-  def word(new_input)
-    @new_input = @input.split("")
-    for x in @new_input do
-      testarray = [x]
-      if testarray.include?("a") || testarray.include?("e") || testarray.include?("i") || testarray.include?("o") ||testarray.include?("u")
-        return "this is a word"
-      end
+  # def word(new_input)
+  #   @new_input = @input.split("")
+  #   for x in @new_input do
+  #     testarray = [x]
+  #     if testarray.include?("a") || testarray.include?("e") || testarray.include?("i") || testarray.include?("o") ||testarray.include?("u")
+  #       return "this is a word"
+  #     end
+  #   end
+  #   return "this is not a word"
+  # end
+  # def anagram(input1, input2)
+  #   @input1 = input1
+  #   @input2 = input2
+  #   @new_input1 = @input1.split("")
+  #   @new_input2 = @input2.split("")
+  #   x = @new_input1
+  #   y = @new_input2
+  #   if x.should =~ y
+  #     return "this is an anagram"
+  #   end
+  #   return "this is not an anagram"
+  # end
+  def antigram(input_antigram1,input_antigram2)
+    @input_antigram1 = input_antigram1
+    @input_antigram2 = input_antigram2
+    @new_input_antigram1 = @input_antigram1.split("")
+    @new_input_antigram2 = @input_antigram2.split("")
+    a = @new_input_antigram1
+    b = @new_input_antigram2
+    p a
+    p b
+    if a.any? != b.any?
+      return "this is an antigram"
     end
-    return "this is not a word"
-  end
-  def anagram(input1, input2)
-    @input1 = input1
-    @input2 = input2
-    @new_input1 = @input1.split("")
-    @new_input2 = @input2.split("")
-    x = @new_input1
-    y = @new_input2
-    if x.should =~ y
-      return "this is an anagram"
-    end
-    return "this is not an anagram"
-  end
-  def antigram()
-    
-
+    return "this is not an antigram"
   end
 end
+
+
+
+
+
+
+
 
   #
   #
--- spec/anagram_and_antigram_spec.rb
+++ spec/anagram_and_antigram_spec.rb
@@ -3,13 +3,16 @@ require('rspec')
 require('Anagram_and_Antigram')
 
 describe('#Anagram_and_Antigram') do
-  it("checks to see if a word contains vowels") do
-    result = Anagram_and_Antigram.new('first')
-    expect(result.word("eat")).to(eq("this is a word"))
+  # it("checks to see if a word contains vowels") do
+  #   result = Anagram_and_Antigram.new('first')
+  #   expect(result.word("eat")).to(eq("this is a word"))
+  # end
+  # it("should check to see if a word is an anagram") do
+  #   result = Anagram_and_Antigram.new('second')
+  #   expect(result.anagram("eat", "tea")).to(eq("this is an anagram"))
+  # end
+  it("should check to see if a word is an antigram") do
+    result = Anagram_and_Antigram.new('third')
+    expect(result.antigram("red", "tai")).to(eq("this is an antigram"))
   end
-  it("should check to see if a word is an anagram") do
-    result = Anagram_and_Antigram.new('second')
-    expect(result.anagram("eat", "tea")).to(eq("this is an anagram"))
-  end
-
 end
