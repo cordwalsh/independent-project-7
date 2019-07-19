c6a647ebfff8251d7943e0dbf48ae6e6317a8188
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-07-19 09:54:08 -0700

    adds method to see if a word is a word

--- lib/anagram_and_antigram.rb
+++ lib/anagram_and_antigram.rb
@@ -1,15 +1,30 @@
 class Anagram_and_Antigram
-  def initialize(word)
-    @word = word
+  def initialize(input, vowels)
+    @input = input
     @vowels = vowels
-    vowels = [a,e,i,o,u]
     @newaray = []
   end
-  def Anagram
-    @new_word = @word.split("")
-    
+  def word
+    @new_input = @input.split("")
+    if @new_input.include?("a","e","i","o","u")
+      puts "this is a word"
+    else
+      puts "this is not a word"
+    end
+
+
+    # vowels = ["a","e","i","o","u"]
+  #   @new_phrase.length.times do |letter|
+  #     @new_array.push(@new_phrase.pop())
+    end
+  #   @new_sentence = @new_array.join("")
+  # end
+  def anagram
+
   end
 
+  def antigram
 
+  end
 
 end
