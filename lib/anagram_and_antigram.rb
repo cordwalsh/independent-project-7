0ead515ddba99db462dcc02bd7c9cb693f25ef38
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-07-19 14:03:47 -0700

    2 tests running

--- lib/anagram_and_antigram.rb
+++ lib/anagram_and_antigram.rb
@@ -3,33 +3,32 @@ class Anagram_and_Antigram
     @input = input
     @newaray = []
   end
-  # def word(new_input)
-  #   @new_input = @input.split("")
-  #   for x in @new_input do
-  #     testarray = [x]
-  #     if testarray.include?("a") || testarray.include?("e") || testarray.include?("i") || testarray.include?("o") ||testarray.include?("u")
-  #       return "this is a word"
-  #     end
-  #   end
-  #   return "this is not a word"
-  # end
+  def word(new_input)
+    @new_input = @input.split("")
+    for x in @new_input do
+      testarray = [x]
+      if testarray.include?("a") || testarray.include?("e") || testarray.include?("i") || testarray.include?("o") ||testarray.include?("u")
+        return "this is a word"
+      end
+    end
+    return "this is not a word"
+  end
   def anagram(input1, input2)
     @input1 = input1
     @input2 = input2
-
     @new_input1 = @input1.split("")
     @new_input2 = @input2.split("")
     x = @new_input1
     y = @new_input2
-    p x
-    p y
-
-    if x.each() == y.each()
+    if x.should =~ y
       return "this is an anagram"
     end
-    return "eh"
+    return "this is not an anagram"
   end
+  def antigram()
+    
 
+  end
 end
 
   #
--- spec/anagram_and_antigram_spec.rb
+++ spec/anagram_and_antigram_spec.rb
@@ -3,12 +3,12 @@ require('rspec')
 require('Anagram_and_Antigram')
 
 describe('#Anagram_and_Antigram') do
-  # it("checks to see if a word contains vowels") do
-  #   result = Anagram_and_Antigram.new('fuck')
-  #   expect(result.word("eat")).to(eq("this is a word"))
-  # end
+  it("checks to see if a word contains vowels") do
+    result = Anagram_and_Antigram.new('first')
+    expect(result.word("eat")).to(eq("this is a word"))
+  end
   it("should check to see if a word is an anagram") do
-    result = Anagram_and_Antigram.new('fuck')
+    result = Anagram_and_Antigram.new('second')
     expect(result.anagram("eat", "tea")).to(eq("this is an anagram"))
   end
 
