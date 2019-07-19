4e07f6b85013d40feb0346b1039e10309e2d739f
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-07-19 16:13:01 -0700

    cleaned up functionality of 4th test

--- lib/anagram_and_antigram.rb
+++ lib/anagram_and_antigram.rb
@@ -3,45 +3,45 @@ class Anagram_and_Antigram
     @input = input
     @newaray = []
   end
-  # def word(new_input)
-  #   @new_input = @input.split("")
-  #   for x in @new_input do
-  #     testarray = [x]
-  #     if testarray.include?("a") || testarray.include?("e") || testarray.include?("i") || testarray.include?("o") ||testarray.include?("u")
-  #       return "this is a word"
-  #     end
-  #   end
-  #   return "this is not a word"
-  # end
-  # def anagram(input1, input2)
-  #   @input1 = input1
-  #   @input2 = input2
-  #   @new_input1 = @input1.upcase.split("")
-  #   @new_input2 = @input2.upcase.split("")
-  #   x = @new_input1
-  #   y = @new_input2
-  #   if x.should =~ y
-  #     return "this is an anagram"
-  #   end
-  #   return "this is not an anagram"
-  # end
-  # def antigram(input_antigram1,input_antigram2)
-  #   @input_antigram1 = input_antigram1
-  #   @input_antigram2 = input_antigram2
-  #   @new_input_antigram1 = @input_antigram1.split("")
-  #   @new_input_antigram2 = @input_antigram2.split("")
-  #   a = @new_input_antigram1
-  #   b = @new_input_antigram2
-  #   p a
-  #   p b
-  #   for x in a do
-  #     p x
-  #     if b.include?(x)
-  #       return "this is not an antigram"
-  #     end
-  #   end
-  #   return "this is an antigram"
-  # end
+  def word(new_input)
+    @new_input = @input.split("")
+    for x in @new_input do
+      testarray = [x]
+      if testarray.include?("a") || testarray.include?("e") || testarray.include?("i") || testarray.include?("o") ||testarray.include?("u")
+        return "this is a word"
+      end
+    end
+    return "this is not a word"
+  end
+  def anagram(input1, input2)
+    @input1 = input1
+    @input2 = input2
+    @new_input1 = @input1.upcase.split("")
+    @new_input2 = @input2.upcase.split("")
+    x = @new_input1
+    y = @new_input2
+    if x.should =~ y
+      return "this is an anagram"
+    end
+    return "this is not an anagram"
+  end
+  def antigram(input_antigram1,input_antigram2)
+    @input_antigram1 = input_antigram1
+    @input_antigram2 = input_antigram2
+    @new_input_antigram1 = @input_antigram1.split("")
+    @new_input_antigram2 = @input_antigram2.split("")
+    a = @new_input_antigram1
+    b = @new_input_antigram2
+    p a
+    p b
+    for x in a do
+      p x
+      if b.include?(x)
+        return "this is not an antigram"
+      end
+    end
+    return "this is an antigram"
+  end
   def multiple_anagrams(input3, input4)
     @input3 = input3
     @input4 = input4
@@ -53,9 +53,10 @@ class Anagram_and_Antigram
     p d
     for x in c do
       if d.include?(x)
+        return "this is not an anagram"
+      elsif c.should =~ d
         return "this is an anagram"
       end
     end
-    return "this is not an anagram"
   end
 end
--- spec/anagram_and_antigram_spec.rb
+++ spec/anagram_and_antigram_spec.rb
@@ -3,20 +3,20 @@ require('rspec')
 require('Anagram_and_Antigram')
 
 describe('#Anagram_and_Antigram') do
-  # it("checks to see if a word contains vowels") do
-  #   result = Anagram_and_Antigram.new('first')
-  #   expect(result.word("eat")).to(eq("this is a word"))
-  # end
-  # it("should check to see if a word is an anagram") do
-  #   result = Anagram_and_Antigram.new('second')
-  #   expect(result.anagram("Eat", "Tea")).to(eq("this is an anagram"))
-  # end
-  # it("should check to see if a word is an antigram") do
-  #   result = Anagram_and_Antigram.new('third')
-  #   expect(result.antigram("red", "tai")).to(eq("this is an antigram"))
-  # end
-  it("should check to see if multiple words is an antigram") do
+  it("checks to see if a word contains vowels") do
+    result = Anagram_and_Antigram.new('first')
+    expect(result.word("eat")).to(eq("this is a word"))
+  end
+  it("should check to see if a word is an anagram") do
+    result = Anagram_and_Antigram.new('second')
+    expect(result.anagram("Eat", "Tea")).to(eq("this is an anagram"))
+  end
+  it("should check to see if a word is an antigram") do
+    result = Anagram_and_Antigram.new('third')
+    expect(result.antigram("red", "tai")).to(eq("this is an antigram"))
+  end
+  it("should check to see if multiple words is an anagram") do
     result = Anagram_and_Antigram.new('fourth')
-    expect(result.multiple_anagrams("my name is cord", "my name is john")).to(eq("this is an anagram"))
+    expect(result.multiple_anagrams("my name is cord", "my name is john")).to(eq("this is not an anagram"))
   end
 end
